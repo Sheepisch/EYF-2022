920103b647a9ba4e0a3a799fb7add4253597ce34
Jeffrey <Jeffrey.beek@hotmail.com>
2022-06-14 09:20:50 +0200

    setup destination info ready

--- public/routes/destination.js
+++ public/routes/destination.js
@@ -2,7 +2,17 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('destination')
+    res.render('destination/destinationHome')
 })
+
+router.get('/weather', (req, res) => {
+    res.render('destination/weather')
+})
+
+router.get('/sights', (req, res) => {
+    res.render('destination/sights')
+})
+
+
 
 module.exports = router
